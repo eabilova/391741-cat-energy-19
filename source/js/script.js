c992183dc192b4e3709a65f44d4851562626b0c2
Elena Abilova <lena.abilova@gmail.com>
2020-05-21 23:16:49 +0300

    Made minor fixes after first check

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,13 @@
+// window.onload = showMenu() {
+//   document.getElementById("menu-button").classList.toggle("show");
+// };
+
+document.getElementById("menu-button").addEventListener("load", function (e) {
+  var target = e.target;
+
+  target.classList.toggle("show");
+}, false)
+
 function openMenu() {
   document.getElementById("main-nav-id").classList.toggle("show");
 }
