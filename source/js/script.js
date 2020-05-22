ae031b04ce6ca3e1eeaba0ba8394be5215f2f516
Elena Abilova <lena.abilova@gmail.com>
2020-05-22 20:47:57 +0300

    Fixed 3.6 and section headers

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,5 @@
-// window.onload = showMenu() {
-//   document.getElementById("menu-button").classList.toggle("show");
-// };
-
-document.getElementById("menu-button").addEventListener("load", function (e) {
-  var target = e.target;
-
-  target.classList.toggle("show");
-}, false)
+document.getElementById("menu-button").classList.toggle("show");
+document.getElementById("main-nav-id").classList.remove("show");
 
 function openMenu() {
   document.getElementById("main-nav-id").classList.toggle("show");
